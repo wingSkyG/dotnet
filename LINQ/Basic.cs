7b64f3f6a96d47c546e4ca7d2824021913a24ff9
wingSky <1392004426@qq.com>
2025-03-20 09:22:45 +0800

    1.初始化项目 2.新建LINQ子项目

---
+++ LINQ/Basic.cs
@@ -0,0 +1,10 @@
+﻿//from、where
+int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var query =
+    from num in numbers
+    where num % 2 == 0 && num > 5
+    select num * 2;
+foreach (var num in query)
+{
+    Console.WriteLine(num);
+}
---
+++ LINQ/group.cs
@@ -0,0 +1,12 @@
+﻿string[] words = ["blueberry", "chimpanzee", "abacus", "banana", "apple", "cheese"];
+var wordGroups =
+    from w in words
+    group w by w[0];
+foreach (var wordGroup in wordGroups)
+{
+    Console.WriteLine("{0}", wordGroup.Key);
+    foreach (var word in wordGroup)
+    {
+        Console.WriteLine(word);
+    }
+}
